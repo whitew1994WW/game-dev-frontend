8d982f55fd241429da0490f5a1404824c21073dc
Will White <willwhite@Wills-MacBook-Pro.local>
2023-12-14 19:44:51 +0000

    Started fronend

---
+++ deploy.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Set the S3 bucket name and CloudFront distribution ID
+BUCKET_NAME=$1
+DISTRIBUTION_ID=$2
+IDENTITY_POOL_ID=$3
+USER_POOL_ID=$4
+USER_POOL_CLIENT_ID=$5
+
+echo "BUCKET_NAME: $BUCKET_NAME"
+echo "DISTRIBUTION_ID: $DISTRIBUTION_ID"
+echo "IDENTITY_POOL_ID: $IDENTITY_POOL_ID"
+echo "USER_POOL_ID: $USER_POOL_ID"
+echo "USER_POOL_CLIENT_ID: $USER_POOL_CLIENT_ID"
+
+
+# Save the Cognito configuration in a JSON file
+echo "Saving Cognito configuration..."
+cat > ./src/amplifyconfiguration.json << EOF
+{
+    "aws_project_region": "us-east-1",
+    "aws_cognito_identity_pool_id": "$IDENTITY_POOL_ID",
+    "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "$USER_POOL_ID",
+    "aws_user_pools_web_client_id": "$USER_POOL_CLIENT_ID",
+    "usernameAttributes": "email"
+}
+EOF
+
+# Build your React application
+echo "Building the React app..."
+npm run build
+
+# Deploy the build to the S3 bucket
+echo "Deploying to S3 bucket..."
+aws s3 sync dist/ s3://$BUCKET_NAME/
+
+# Invalidate the CloudFront cache to ensure fresh content is served
+echo "Invalidating CloudFront cache..."
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*"
+
+# Retrieve and print the CloudFront Distribution URL
+CLOUDFRONT_URL=$(aws cloudfront get-distribution --id $DISTRIBUTION_ID --query "Distribution.DomainName" --output text)
+echo "Your CloudFront web address is: https://$CLOUDFRONT_URL"
+
+# Instal jq
